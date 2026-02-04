7196e3e252bbde646aa2ac9e59c665443e2a44e5
Calin <nilac8991@gmail.com>
2026-02-04 12:31:38 +0100

    Add MainViewModel skeleton class
    Signed-off-by: Calin <nilac8991@gmail.com>

---
+++ app/src/main/java/com/example/zebra/zaidcsdktextocrsample/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.zebra.zaidcsdktextocrsample.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(applicationContext: Application) : AndroidViewModel(applicationContext) {
+
+    companion object {
+        const val TAG = "MainViewModel"
+    }
+}
