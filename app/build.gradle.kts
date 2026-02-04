3b2af0f0a956f7ff5a6cd3c68c885ba9b70c7aa2
Calin <nilac8991@gmail.com>
2026-02-04 11:17:07 +0100

    Add required dependencies
    Signed-off-by: Calin <nilac8991@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,19 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    implementation(libs.camera.core)
+    implementation(libs.camera.camera2)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
+    implementation(libs.zebra.ai.vision.sdk) { artifact { type = "aar" } }
+    implementation(libs.text.ocr.recognizer) { artifact { type = "aar" } }
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
